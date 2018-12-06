3213efa22a9850d74fa0707da01fb5e56dc2ae3e
lijun <lijun@bjgoodwill.com>
2018-12-06 11:00:36 +0800

    1、在BaseController增加分页相关注释 2、增加Mybatis执行SQL的debug日志输出到控制台

--- src/main/java/com/bjgoodwill/isteam/common/controller/BaseController.java
+++ src/main/java/com/bjgoodwill/isteam/common/controller/BaseController.java
@@ -51,8 +51,12 @@ public class BaseController {
     }
 
     protected Map<String, Object> selectByPageNumSize(QueryRequest request, Supplier<?> s) {
+        // 执行PageHelper.startPage后，下一个Mybatis查询语句执行时会被处理为分页操作，即返回的不是List，而是Page
         PageHelper.startPage(request.getPageNum(), request.getPageSize());
+        // s.get()方法执行时即进入DAO层执行sql，如果此时打断点，第一次执行s.get()时返回Page,第二次返回List
         PageInfo<?> pageInfo = new PageInfo<>((List<?>) s.get());
+        // 以下操作是为了保险起见，避免下一个查询操作受到上一次查询的影响。其实在s.get()执行完毕后，执行了同样的操作
+        // 操作方法见PageInterceptor.intercept,在try catch的finally中进行了清除当前线程变量
         PageHelper.clearPage();
         return getDataTable(pageInfo);
     }
